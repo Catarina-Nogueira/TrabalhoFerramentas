e2ed9df8a6f2277c7b9946704b345e004089440f
Catarina-Nogueira <anaacatarina.cn@gmail.com>
2025-02-22 14:43:44 +0000

    first commit

---
+++ src/Cilindro.java
@@ -0,0 +1,42 @@
+/*
+Uma classe é uma representação de uma entidade que serve de modelo para o objeto.
+Classes:
+- de objeto ou de instância: designam atributos para cada objeto
+- de classe ou estáticas: não precisam de criar objetos
+- locais
+
+*/
+public class Cilindro {
+
+        private double altura;
+        private double raio; /*o uso do modificador de acesso private permite o encapsulamento da classe - não se acede diretamente
+        Definição de encapsulamento: Capacidade de ocultar o estado interno e as funcionalidades de um objeto permitindo apenas o acesso através de um conjunto público de métodos.
+
+*/
+        //atributos com o mesmo conjunto de comportamentos
+        //o que distingue dois objetos da classe é o valor dos atributos dos objetos - o estado dos objetos
+
+        public double getRaio() { // método de acesso que permite aceder aos valores dos atributos
+                return raio;
+        }
+
+        public double getAltura() { //método de acesso
+                return altura;
+        }
+
+        public void setRaio(double raio) { //método de modificação
+                this.raio = raio;
+        }
+
+        public void setAltura(double altura) {
+                this.altura = altura; // "this" é uma referência para o próprio objeto - "este objeto" - para evitar ambiguidade, para não usar o this, pode-se mudar o nome da variável passada por parâmetro
+        }
+
+        public double calcularVolume(){ //não leva valores por parâmetro porque usa os valores dos atributos
+                return Math.PI * Math.pow(raio,2) * altura;
+                //ou também se pode criar uma variável local vol apenas return vol = Math.PI * Math.pow(raio,2) * altura; return vol;
+        }
+
+        //estes três métodos get e set e calcularVolume são métodos de instância que variam consoante a situação
+
+}
---
+++ src/Main.java
@@ -0,0 +1,33 @@
+/* Paradigma de Programação orientada ao Objeto - modelo sequencial de programação
+Tem como base pensar e caracterizar um objeto.
+
+Um objeto é uma entidade física e única, conceptual do munfo real.
+
+A interação entre objetos dá-se através de um dispositivo de mensagens. Um objeto envia (objeto emissor) a mensagem a um objeto (recetor) para que este execute o pedido que lhe foi diretamente dirigido.
+
+Deste modo um objeto traz consigo atributos (variáveis) privados e funcionalidades / comportamentos (métodos).
+
+Atenção, os métodos não devem ter System.out.print
+
+A instaciação do objeto segue sempre o modelo: Nome da classe   Nome do objeto  =  new    Nome da Classe ();
+
+*/
+public class Main {
+    public static void main(String[] args) {
+        Cilindro c1= new Cilindro(); // instanciar o objeto - criação do objeto da classe Cilindro; o "c1" é uma variável local ao método main
+        Cilindro c2 = new Cilindro();
+        double v1;
+        double v2;
+
+        c1.setRaio(5);
+        c1.setAltura(10); //refência para o objeto "c1" sobre o método de instância set
+        v1 = c1.calcularVolume();
+
+        c2.setRaio(4);
+        c2.setAltura(16); //refência para o objeto "c2" sobre o método de instância set
+        v2 = c2.calcularVolume(); //invocação de métodos
+
+        System.out.println("Volume de c1: " + v1);
+        System.out.println("Volume de c2: " + v2);
+    }
+}
